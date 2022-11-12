c2981ac604936aebf21bbb14dad5f842651a1fbd
astrojose <jossey1918@gmail.com>
2022-11-12 04:39:06 +0300

    start styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,10 @@ interface Props {
 }
 const Layout  = ({children} : Props) => {
   return (
-    <div className="max-w-xl  mx-auto max-h-screen bg-gray-400">
+    <div
+      className="max-w-xl my-12 mx-auto max-h-screen bg-white">
       <div className="flex justify-center items-center h-16">
-        <h1>Pangilia Todo</h1>
+        <h1 className="font-bold text-2xl">Pangilia Todo</h1>
       </div>
       <div> { children }</div>
     </div>
